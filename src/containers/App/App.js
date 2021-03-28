bbd9407efb50f53d8ed82bbbe4c5e0abda243bab
unknown <phamhande@gmail.com>
2021-03-29 03:17:41 +0700

    base folder

---
+++ src/containers/App/App.js
@@ -0,0 +1,13 @@
+/**
+Mr : Pham Phai Ha
+Email: happ@runsystem.net
+ */
+import React from 'react';
+
+function App() {
+    return (
+        <div>Hello</div>
+    );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+/**
+Mr : Pham Phai Ha
+Email: happ@runsystem.net
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App/App';
+
+const ROOT = document.getElementById('root');
+ReactDOM.render(<App />, ROOT);
